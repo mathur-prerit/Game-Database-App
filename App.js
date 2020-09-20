72171b499fdd3046ddca5e9b544575725ea6a371
Prerit Mathur <mathur.prerit@gmail.com>
2020-09-20 22:03:23 +0530

    added all useful component

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text, View, Button, TextInput, ScrollView,FlatList,TouchableOpacity,Alert,TouchableWithoutFeedback,
+} from 'react-native';
 import { styles } from './src/assests/styling/master-style';
 
 export default function App() {
+  const [name, setName] = useState('Prerit');
+
   return (
+    <TouchableWithoutFeedback onPress={()=>keyboard.dismiss()}
     <View style={styles.container}>
-      <Text>Welcome to Game DB</Text>
       <StatusBar style="auto" />
+  <Text>Welcome {name} to Game DB</Text>
+      <View>
+        <TextInput keyboardType='numeric' multiline placeholder="Your Name" onChangeText={(val) => {
+          setName(val);
+        }}/>
+        <Button title="lets see games" onPress={() => {
+          Alert.alert(`Welcome ${name}`);
+        }}/>
+      </View>
     </View>
+    <ScrollView>
+      <View>
+        data.map(item=>(
+          <Text></Text>
+        ))
+      </View>
+    </ScrollView>
+
+    <FlatList 
+    numColumns={2}
+    keyExtractor={(item)=>item.id}
+    data={people} renderItem=({item})=>(
+      <TouchableOpacity onPress={()=>pressHandlerfunc(id)}>
+      <Text>fdfd</Text>
+      </TouchableOpacity>
+    )/>
   );
 }

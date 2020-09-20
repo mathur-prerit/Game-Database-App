61c62c20acec69585f59e98d92ab2af52770af4f
Prerit Mathur <mathur.prerit@gmail.com>
2020-09-20 22:24:43 +0530

    basic component structure learned

--- App.js
+++ App.js
@@ -4,9 +4,30 @@ import {
   Text, View, Button, TextInput, ScrollView,FlatList,TouchableOpacity,Alert,TouchableWithoutFeedback,
 } from 'react-native';
 import { styles } from './src/assests/styling/master-style';
+import * Font as './2243  '
+import {AppLoading} from 'expo'
+
+
+
+const getFonts=()=>{
+  return Font.loadAsync({
+    font-name=require(path),
+
+  })
+}
 
 export default function App() {
   const [name, setName] = useState('Prerit');
+
+  if(name){
+    return <ada></ada>
+  }else{
+    <AppLoading
+    startAsync={getFonts}
+    onFinish={()=>setName('23322')}/>
+  }
+
+
 
   return (
     <TouchableWithoutFeedback onPress={()=>keyboard.dismiss()}
